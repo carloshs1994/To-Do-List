8931d3a0d5e0f1b85a8254bf0133d89359cb4cd7
Melanie Arellano <melaniesigrid@protonmail.com>
2022-01-21 09:38:37 -0600

    Modify updateListArray test to make sure it updates the source array, change to camelCase

--- src/__test__/checkobox-status.test.js
+++ src/__test__/checkobox-status.test.js
@@ -1,22 +1,23 @@
 import {
-  clearCheckedTasks,
-  checkboxStatus,
+  // clearCheckedTasks,
+  // checkboxStatus,
   updateListArray,
 } from '../checkbox-status.js';
 
 test('should update the describe value inside listOfTasks', () => {
-  const DotMenu = './ellipsis-v-solid.svg';
+  const dotMenu = './ellipsis-v-solid.svg';
   const listOfTask = [{
     description: 'Run 1 km',
     completed: false,
     index: 0,
   }];
-  const KeboardEvent = {
+  const keyboardEvent = {
     keyCode: 13,
     target: {
+      id: 0,
+      value: 'Run 3 km',
       parentElement: {
-        children: [
-          {
+        children: [{
             input: {
               id: 'task',
             },
@@ -33,9 +34,10 @@ test('should update the describe value inside listOfTasks', () => {
           },
         ],
       },
-      id: '0',
     },
   };
-  updateListArray(DotMenu, KeboardEvent, listOfTask);
-  expect(KeboardEvent.target.parentElement.children[2].src).toEqual('./ellipsis-v-solid.svg');
+  const indexOfChanged = keyboardEvent.target.id;
+  updateListArray(dotMenu, keyboardEvent, listOfTask);
+  expect(keyboardEvent.target.parentElement.children[2].src).toEqual('./ellipsis-v-solid.svg');
+  expect(listOfTask[indexOfChanged].description).toEqual('Run 3 km');
 });
