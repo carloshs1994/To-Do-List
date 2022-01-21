3c384021bac6564ed5e589749a9b36e7e53db57a
Melanie Arellano <melaniesigrid@protonmail.com>
2022-01-21 10:17:30 -0600

    Start checked test

--- src/__test__/checkobox-status.test.js
+++ src/__test__/checkobox-status.test.js
@@ -1,43 +1,73 @@
 import {
   // clearCheckedTasks,
-  // checkboxStatus,
+  checkboxStatus,
   updateListArray,
 } from '../checkbox-status.js';
-
-test('should update the describe value inside listOfTasks', () => {
+describe('List status updates', () => {
   const dotMenu = './ellipsis-v-solid.svg';
   const listOfTask = [{
     description: 'Run 1 km',
     completed: false,
     index: 0,
   }];
-  const keyboardEvent = {
-    keyCode: 13,
-    target: {
-      id: 0,
-      value: 'Run 3 km',
-      parentElement: {
-        children: [{
-            input: {
-              id: 'task',
+  test('should update the describe value inside listOfTasks', () => {
+    const keyboardEvent = {
+      keyCode: 13,
+      target: {
+        id: 0,
+        value: 'Run 3 km',
+        parentElement: {
+          children: [{
+              input: {
+                id: 'task',
+              },
             },
-          },
-          {
-            input: {
-              id: 4,
-              class: 'task-test',
+            {
+              input: {
+                id: 4,
+                class: 'task-test',
+              },
             },
-          },
-          {
-            class: 4,
-            src: './trash-alt-regular.svg',
-          },
-        ],
+            {
+              class: 4,
+              src: './trash-alt-regular.svg',
+            },
+          ],
+        },
       },
-    },
-  };
-  const indexOfChanged = keyboardEvent.target.id;
-  updateListArray(dotMenu, keyboardEvent, listOfTask);
-  expect(keyboardEvent.target.parentElement.children[2].src).toEqual('./ellipsis-v-solid.svg');
-  expect(listOfTask[indexOfChanged].description).toEqual('Run 3 km');
+    };
+    const indexOfChanged = keyboardEvent.target.id;
+    updateListArray(dotMenu, keyboardEvent, listOfTask);
+    expect(keyboardEvent.target.parentElement.children[2].src).toEqual('./ellipsis-v-solid.svg');
+    expect(listOfTask[indexOfChanged].description).toEqual('Run 3 km');
+  });
+  test('Complete items', () => {
+    const checkBoxEvent = {
+      target: {
+        id: 0,
+        checked: true,
+        parentNode: {
+          children: [{
+              input: {
+                id: 'task',
+              },
+            },
+            {
+              input: {
+                id: 0,
+                classList: {
+                  toggle: ''
+                },
+              },
+            },
+          ],
+        },
+      },
+    };
+
+    checkboxStatus(checkBoxEvent, listOfTask);
+    const indexOfCheckbox = checkBoxEvent.target.id;
+    console.log(checkBoxEvent.target.parentNode.children[1]);
+    expect(listOfTask[indexOfCheckbox].completed).toEqual(true);
+  });
 });
